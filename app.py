893d04366670bacc9ae3aad5d7640a2d0e3cf50f
David Weatherstone <davidweatherstone@gmail.com>
2020-07-06 20:45:35 +0100

    update to .gitignore

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def index():
                 no_stop_words_count.items(),
                 key=operator.itemgetter(1),
                 reverse=True
-            )[:15]
+            )[:10]
             try:
                 result = Result(
                     url=url,
