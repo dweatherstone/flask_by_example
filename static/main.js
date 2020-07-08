c77a59c87af2bdbb1880a209cbd89edd7c406d9d
David Weatherstone <davidweatherstone@gmail.com>
2020-07-08 15:48:15 +0100

    Lesson 7: making the UI nicer

--- static/main.js
+++ static/main.js
@@ -5,6 +5,9 @@
 
         .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
             function ($scope, $log, $http, $timeout) {
+                $scope.submitButtonText = 'Submit';
+                $scope.loading = false;
+                $scope.urlerror = false;
                 $scope.getResults = function () {
                     $log.log("test");
 
@@ -16,6 +19,10 @@
                         success(function (results) {
                             $log.log(results);
                             getWordCount(results);
+                            $scope.wordcounts = null;
+                            $scope.loading = true;
+                            $scope.submitButtonText = "Loading...";
+                            $scope.urlerror = false;
                         }).
                         error(function (error) {
                             $log.log(error);
@@ -32,6 +39,8 @@
                                     $log.log(data, status);
                                 } else if (status === 200) {
                                     $log.log(data);
+                                    $scope.loading = false;
+                                    $scope.submitButtonText = "Submit";
                                     $scope.wordcounts = data;
                                     $timeout.cancel(timeout);
                                     return false;
@@ -39,6 +48,11 @@
                                 // continue to call the poller() function every 2 seconds
                                 // until the timeout is cancelled
                                 timeout = $timeout(poller, 2000);
+                            }).error(function(error) {
+                                $log.log(error);
+                                $scope.loading = false;
+                                $scope.submitButtonText = "Submit";
+                                $scope.urlerror = true;
                             });
                     };
                     poller();
